3eb566e0fb7bcd0ec62c3235abd4ec1c82d3f336
Joanna Gołofit <jgolofit@future-processing.com>
2022-10-12 14:18:07 +0200

    114. Using the useEffect Cleanup Function

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,17 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+    const identifier = setTimeout(() => {
+      console.log("validating");
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 1000)
+    return () => {
+      console.log("cleanup");
+      clearTimeout(identifier);
+
+    }
   }, [enteredEmail, enteredPassword])
 
 
