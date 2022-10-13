c5b8c282f51f20f40dde7b582cd86bfacb05251f
Joanna Gołofit <jgolofit@future-processing.com>
2022-10-13 15:37:53 +0200

    createContext, using exported  provider and putting all logic in there

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,9 +1,37 @@
-import { createContext } from 'react'
+import { createContext, useEffect, useState } from 'react'
 
 const AuthContext = createContext({
   //it's just a dummy information, mainly showing what properties your object has, and vsc will show it in 'autocompletion'
   isLoggedIn: false,
-  onLogout: () => {}
+  onLogout: () => {},
+  onLogin: (email, password) => {}
 })
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    let storedLoggedInfo = localStorage.getItem("isLoggedIn");
+    storedLoggedInfo === "1" && setIsLoggedIn(true);
+  }, [])
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1")
+    setIsLoggedIn(true);
+  };
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn")
+    setIsLoggedIn(false);
+  };
+
+
+  return <AuthContext.Provider value={{
+    isLoggedIn: isLoggedIn,
+    onLogout: logoutHandler,
+    onLogin: loginHandler
+  }}> {props.children} </AuthContext.Provider>
+}
 
 export default AuthContext
