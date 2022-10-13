4970aff7cd08e9c1d21e566762d44d7749063979
Joanna Gołofit <jgolofit@future-processing.com>
2022-10-13 15:15:02 +0200

    updating default context

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,9 @@
 import { createContext } from 'react'
 
 const AuthContext = createContext({
-  isLoggedIn: false
+  //it's just a dummy information, mainly showing what properties your object has, and vsc will show it in 'autocompletion'
+  isLoggedIn: false,
+  onLogout: () => {}
 })
 
 export default AuthContext
