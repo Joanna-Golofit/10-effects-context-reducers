e1c398450a8a8696d27f3a26e89e5061bef2e321
Joanna Gołofit <jgolofit@future-processing.com>
2022-10-13 15:45:57 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -7,24 +7,6 @@ import AuthContext from './store/auth-context';
 function App() {
   const ctx = useContext(AuthContext)
 
-  // useEffect(() => {
-  //   let storedLoggedInfo = localStorage.getItem("isLoggedIn");
-  //   storedLoggedInfo === "1" && setIsLoggedIn(true);
-  // }, [])
-
-
-  // const loginHandler = (email, password) => {
-  //   // We should of course check email and password
-  //   // But it's just a dummy/ demo anyways
-  //   localStorage.setItem("isLoggedIn", "1")
-  //   setIsLoggedIn(true);
-  // };
-
-  // const logoutHandler = () => {
-  //   localStorage.removeItem("isLoggedIn")
-  //   setIsLoggedIn(false);
-  // };
-
   return (
     <>
       <MainHeader />
